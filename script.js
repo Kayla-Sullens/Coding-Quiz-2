c44b55e7c63873868ffdf2b51a0d2b41428b486e
Kayla <sullenspartyof3@gmail.com>
2023-08-21 12:52:17 -0700

    Questions added in JS

---
+++ script.js
@@ -0,0 +1,28 @@
+// Var with array and object for quiz questions
+var questions = [
+    {
+        question: "How do you add a comment to a CSS or JS file?",
+        choices: ["*Comment*", "//Comment", "/*Comment", "--Comment--"],
+        answer: "//Comment"
+    },
+    {
+        question: "What is the first index of an array?",
+        choices: ["0", "1", "First", "Idk"],
+        answer: "0"
+    },
+    {
+        question: "Which CSS property gives something rounded corners?",
+        choices: ["corner-round", "rounding", "border-radius", "border"],
+        answer: "border-radius"
+    },
+    {
+        question: "What is a short section of code written to complete a task called?",
+        choices: ["Function", "Array", "Loop", "Variable"],
+        answer: "Function"
+    },
+    {
+        question: "String values must be enclosed with ___",
+        choices: ["parenthesis", "quotes", "commas", "curly brackets"],
+        answer: "quotes"
+    },
+];
